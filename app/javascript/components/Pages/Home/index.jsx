a046d618fdfddcc2f072bed5205244215a348bc9
Chris Evans <evanschris@users.noreply.github.com>
2019-08-04 17:49:20 +0100

    add in strapline from CMS

--- app/javascript/components/Pages/Home/index.jsx
+++ app/javascript/components/Pages/Home/index.jsx
@@ -2,11 +2,11 @@ import React from "react"
 import Layout from "../../Layout"
 import Hero from "../../Hero"
 
-export default ({title})=>
+export default ({title, strapline})=>
   <Layout withFooter>
     <Hero
       title={title}
-      description="Helping you find the right information and support in Buckinghamshire."
+      description={strapline}
       ctaTitle="Find activities, groups and services near you"
       ctaDescription="Answer a few questions and we'll suggest recommendations in your area"
       ctaHref="/explore-your-needs"
