9c2dfe6f16e972ae3224349f9a99331edaf917b4
Chris Evans <evanschris@users.noreply.github.com>
2019-08-12 17:18:43 +0100

    redirect admin to pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   namespace :admin do
     resources :lifestyle_sections
+    get '/', to: redirect('/admin/pages')
   end
   # mount Fae below your admin namespec
   mount Fae::Engine => '/admin'
