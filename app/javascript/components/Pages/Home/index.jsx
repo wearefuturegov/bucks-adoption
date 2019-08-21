8941b3f2ee7beb38da2152ffa24287eafd712d1e
Chris Evans <evanschris@users.noreply.github.com>
2019-08-21 11:03:34 +0100

    update homepage and add markdown parser

--- app/javascript/components/Pages/Home/index.jsx
+++ app/javascript/components/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import Markdown from 'markdown-to-jsx';
+import { render } from 'react-dom';
 import Layout from "../../Layout"
 import Hero from "../../Hero"
 import TopicPageContent from "../../TopicPageContent"
 import "./style.scss"
 
-export default ({title, strapline, cta_title, cta_text, cta_button, lifestyletitle, lifestyleintro})=>
+export default ({title, strapline, body, cta_title, cta_text, cta_button, lifestyletitle, lifestyleintro})=>
   <Layout withFooter>
     <Hero
       title={title}
@@ -17,7 +19,11 @@ export default ({title, strapline, cta_title, cta_text, cta_button, lifestyletit
 
     <div className="topic-sections">
       <div className="container">
-        <p>Possibly a navigation section for the 5 topics here.</p>
+        {
+          <Markdown>{body}</Markdown>
+        }
+        { // navigation section for the 5 topics here
+        }
         <br/>
         <br/>
 
