7be9b0c7438555b742b182846301365c150ce8ff
Chris Evans <evanschris@users.noreply.github.com>
2019-08-22 15:41:39 +0100

    add checkmark to completed sections

--- app/javascript/components/TopicPageComplete/index.jsx
+++ app/javascript/components/TopicPageComplete/index.jsx
@@ -6,8 +6,9 @@ import Button from '../Button'
 const TopicPageComplete = ({title, intro, link, resultsStored }) => {
   return(
     <section className="topic-page-content topic-questions-compete">
-      <TopicCard key="1" intro={intro + "<br/><br/> You have completed the " + title + " section."} title={title} type={link}>
+      <TopicCard key="1" intro={intro + "<br/><br/> **You have completed the " + title + " section.**"} title={title} type={link}>
          <a className="float-right-text" href={link + "/index"}>See your results</a>
+         <div className="checkmark_container"><div className="checkmark"></div></div>
       </TopicCard>
     </section>
   )
