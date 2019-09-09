440f1ee2aa6a4576990bf0f752c7e504f9ff7974
Chris Evans <evanschris@users.noreply.github.com>
2019-09-09 22:10:11 +0100

    remove superflous container div

--- app/javascript/components/Pages/ResultsPage/index.jsx
+++ app/javascript/components/Pages/ResultsPage/index.jsx
@@ -74,59 +74,57 @@ const ResultsPage = ({
         ):null
         }
         <PageBodyContent>
-          <div className="container">
-            <div className="health-results-summary">
-              {
-                healthResultsStored.length ? (
-                  <TopicResultsSummary withTitle topicSection={health_title} resultsArray={healthResultsStored.split('')} questions={health_questions} type="results_topic_lifestyle" />
-                ) : (
-                  <TopicResultsSummary withTitle topicSection={health_title} questions={health_questions} type="results_topic_lifestyle" link="health" />
-                )
-              }
-            </div>
-            <div className="time-results-summary">
-              {
-                timeResultsStored.length ? (
-                  <TopicResultsSummary withTitle topicSection={time_title} resultsArray={timeResultsStored.split('')} questions={time_questions} type="results_topic_time" />
-                ) : (
-                  <TopicResultsSummary withTitle topicSection={time_title} questions={time_questions} type="results_topic_time" link="time" />
-                )
-              }
-            </div>
-            <div className="family-results-summary">
-              {
-                familyResultsStored.length ? (
-                  <TopicResultsSummary withTitle topicSection={family_title} resultsArray={familyResultsStored.split('')} questions={family_questions} type="results_topic_family" />
-                ) : (
-                  <TopicResultsSummary withTitle topicSection={family_title} questions={family_questions} type="results_topic_family" link="family" />
-                )
-              }
-            </div>
-            <div className="skills-results-summary">
-              {
-                skillsResultsStored.length ? (
-                  <TopicResultsSummary withTitle topicSection={skills_title} resultsArray={skillsResultsStored.split('')} questions={skills_questions} type="results_topic_skills" />
-                ) : (
-                  <TopicResultsSummary withTitle topicSection={skills_title} questions={skills_questions} type="results_topic_skills" link="skills" />
-                )
-              }
-            </div>
-            <div className="home-results-summary">
-              {
-                homeResultsStored.length ? (
-                  <TopicResultsSummary withTitle topicSection={home_title} resultsArray={homeResultsStored.split('')} questions={home_questions} type="results_topic_home" />
-                ) : (
-                  <TopicResultsSummary withTitle topicSection={home_title} questions={home_questions} type="results_topic_home" link="home" />
-                )
-              }
-            </div>
+          <div className="health-results-summary">
+            {
+              healthResultsStored.length ? (
+                <TopicResultsSummary withTitle topicSection={health_title} resultsArray={healthResultsStored.split('')} questions={health_questions} type="results_topic_lifestyle" />
+              ) : (
+                <TopicResultsSummary withTitle topicSection={health_title} questions={health_questions} type="results_topic_lifestyle" link="health" />
+              )
+            }
+          </div>
+          <div className="time-results-summary">
+            {
+              timeResultsStored.length ? (
+                <TopicResultsSummary withTitle topicSection={time_title} resultsArray={timeResultsStored.split('')} questions={time_questions} type="results_topic_time" />
+              ) : (
+                <TopicResultsSummary withTitle topicSection={time_title} questions={time_questions} type="results_topic_time" link="time" />
+              )
+            }
+          </div>
+          <div className="family-results-summary">
+            {
+              familyResultsStored.length ? (
+                <TopicResultsSummary withTitle topicSection={family_title} resultsArray={familyResultsStored.split('')} questions={family_questions} type="results_topic_family" />
+              ) : (
+                <TopicResultsSummary withTitle topicSection={family_title} questions={family_questions} type="results_topic_family" link="family" />
+              )
+            }
+          </div>
+          <div className="skills-results-summary">
+            {
+              skillsResultsStored.length ? (
+                <TopicResultsSummary withTitle topicSection={skills_title} resultsArray={skillsResultsStored.split('')} questions={skills_questions} type="results_topic_skills" />
+              ) : (
+                <TopicResultsSummary withTitle topicSection={skills_title} questions={skills_questions} type="results_topic_skills" link="skills" />
+              )
+            }
+          </div>
+          <div className="home-results-summary">
+            {
+              homeResultsStored.length ? (
+                <TopicResultsSummary withTitle topicSection={home_title} resultsArray={homeResultsStored.split('')} questions={home_questions} type="results_topic_home" />
+              ) : (
+                <TopicResultsSummary withTitle topicSection={home_title} questions={home_questions} type="results_topic_home" link="home" />
+              )
+            }
+          </div>
 
-            <div className="other-results">
-              <h2>Other things to do</h2>
-              <TopicCard title="Come to an adoption evening" intro="We recommend the next step you take is to come to one of our regularly run information evenings. At the event, you will be able to ask our team questions and will hear from our team and previous adopters.">
-                <a href="/pages/bookadoptionevening">Attend an evening</a>
-              </TopicCard>
-            </div>
+          <div className="other-results">
+            <h2>Other things to do</h2>
+            <TopicCard title="Come to an adoption evening" intro="We recommend the next step you take is to come to one of our regularly run information evenings. At the event, you will be able to ask our team questions and will hear from our team and previous adopters.">
+              <a href="/pages/bookadoptionevening">Attend an evening</a>
+            </TopicCard>
           </div>
         </PageBodyContent>
       </div>
