ff18e695641be66bfa3155a2cd9720d04d2d77da
userman123 <dominic.freeman123@gmail.com>
2019-09-27 11:00:21 +0100

    Remove reply to ID as is configured on Notify

--- app/mailers/share_mailer.rb
+++ app/mailers/share_mailer.rb
@@ -6,8 +6,7 @@ class ShareMailer < Mail::Notify::Mailer
     view_mail(
       ENV['GOVUK_NOTIFY_TEMPLATE_ID'],
       to: @recipient,
-      subject: 'Your Adoption Ready Plan',
-      reply_to_id: ENV['GOVUK_NOTIFY_REPLY_TO_ID']
+      subject: 'Your Adoption Ready Plan'
     )
   end
 end
