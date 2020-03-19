094f3b52fb789a8605232a7010741247a7f82ed0
Chris Evans <evanschris@users.noreply.github.com>
2020-03-19 15:21:22 +0000

    update footer

--- app/javascript/components/Footer/index.jsx
+++ app/javascript/components/Footer/index.jsx
@@ -2,25 +2,16 @@ import React from "react"
 import styled from "styled-components"
 import theme from "../_theme"
 import Cookies from "../Cookies"
+import facebook from "./facebook.svg"
+import twitter from "./twitter.svg"
+import youtube from "./youtube.svg"
 
 const Outer = styled.footer`
-    background: ${theme.shadow};
-    padding: 40px 20px;
-    font-size: 0.9em;
-    color: ${theme.lightText};
-    @media screen and (min-width: ${theme.tablet}){
-        padding: 60px 20px;
-        font-size: 0.95em;
-    }
-    a{
-        color: ${theme.lightText};
-        &:hover{
-            text-decoration: none;
-        }
-        &:focus{
-            outline: 3px solid ${theme.focus};
-            background: ${theme.focus};
-        }
+    background: ${theme.grey1};
+    color: white;
+    padding: 30px 10px;
+    @media screen and (min-width: 600px){
+        padding: 40px 10px;
     }
 `
 
@@ -30,29 +21,80 @@ const Inner = styled.div`
     margin-right: auto;
 `
 
-const Menu = styled.ul`
-    list-style: none;
-    margin-bottom: 15px;
+const FooterNav = styled.nav`
+    margin-bottom: 30px;
+    @media screen and (min-width: 600px){
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 50px;
+    }
 `
 
-const MenuItem = styled.li`
-    display: inline-block;
-    margin-right: 20px;
-    margin-bottom: 10px;
+const FooterLinks = styled.div`
+    margin-bottom: 15px;
+    @media screen and (min-width: 600px){
+        margin-bottom: 0px;
+    }
 `
+
+const FooterLink = styled.a`
+    color: white;
+    margin-right: 10px;
+    text-decoration: none;
+    &:hover{
+        text-decoration: underline;
+    }
+    &:focus{
+        color: ${theme.grey1};
+        background: ${theme.focus};
+        outline: 3px solid ${theme.focus};
+    }
+    @media screen and (min-width: 600px){
+        margin-right: 20px;
+    }
+`
+
+const SocialLink = styled.a`
+    margin-right: 10px;
+    &:focus{
+        outline: 3px solid ${theme.focus};
+    }
+    @media screen and (min-width: 600px){
+        margin-right: 20px;
+    }
+`
+
+const SocialIcon = styled.img`
+    width: 25px;
+`
+
+const CopyrightNotice = styled.p``
 
 const Footer = () =>
     <>
     <Outer>
         <Inner>
-            <Menu>
-                <MenuItem><a href="https://www.buckscc.gov.uk/services/contact-and-complaints/contact-us/">Contact us</a></MenuItem>
-                <MenuItem><a href="/pages/cookies">Cookies</a></MenuItem>
-                <MenuItem><a href="https://www.buckscc.gov.uk/services/council-and-democracy/privacy-policy/">Privacy statement</a></MenuItem>
-                <MenuItem><a href="/">Accessibility statement</a></MenuItem>
-                <MenuItem><a href="https://www.buckscc.gov.uk/services/community/community-safety/modern-slavery/bcc-modern-slavery-statement/">Modern slavery statement</a></MenuItem>
-            </Menu>
-            <p>Built and maintained by <a href="http://buckscc.gov.uk">Buckinghamshire County Council</a></p>
+            <FooterNav>
+                <FooterLinks>
+                    <FooterLink href="https://www.buckscc.gov.uk/services/council-and-democracy/cookies/">Cookies</FooterLink>
+                    <FooterLink href="https://www.buckscc.gov.uk/services/council-and-democracy/privacy-policy/">Privacy and data</FooterLink>
+                    <FooterLink href="https://www.buckscc.gov.uk/services/contact-and-complaints/contact-us/">Contact us</FooterLink>
+                </FooterLinks>
+                <div>
+                    <SocialLink href="https://www.facebook.com/BuckinghamshireCountyCouncil/">
+                        <SocialIcon src={facebook} alt="Facebook"/>
+                    </SocialLink>
+                    <SocialLink href="http://twitter.com/buckscc">
+                        <SocialIcon src={twitter} alt="Twitter"/>
+                    </SocialLink>
+                    <SocialLink href="http://www.youtube.com/buckinghamshirecc">
+                        <SocialIcon src={youtube} alt="YouTube"/>
+                    </SocialLink>
+                </div>
+            </FooterNav>
+            <CopyrightNotice>© 2019 Buckinghamshire Council</CopyrightNotice>
         </Inner>
     </Outer>
     <Cookies/>
